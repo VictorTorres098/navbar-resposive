db630d988fd4002a4dfc190a48c292f4c611025d
bach <byebachgm@gmail.com>
2022-05-19 16:09:31 -0300

    funciones onClick

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,8 +1,14 @@
-import React from 'react'
 import styled from 'styled-components';
+import BurgerButton from '../burgerbutton/BurgerButton';
 import ButtonL from '../button/ButtonL';
+import React, {useState} from 'react';
 
 export const NavBar = () => {
+    const[cliked, setCliked] = useState(false);
+    const handleClick = () => {
+        //cuando esta true lo passa a false y viceversa
+        setCliked(!cliked);
+    }
   return (
     <>
         <NavConteiner>
@@ -10,7 +16,7 @@ export const NavBar = () => {
                 <h1>Léete La Vida</h1>
             </div>
 
-            <div className='links'>
+            <div className={`links ${cliked ? 'active' : '' }`}>
                 <a href="/">Home</a>
                 <a href="/">Reseñas</a>
                 <a href="/">Mi espacio</a>
@@ -20,7 +26,11 @@ export const NavBar = () => {
 
             <div className='nav-login'>
                 <ButtonL buttonText="Login" />
-            </div>           
+            </div>
+            <div className='burger'>
+                <BurgerButton cliked={cliked} handleClick={handleClick} />
+            </div>
+                       
         </NavConteiner>
     
     </>
@@ -48,9 +58,45 @@ const NavConteiner = styled.nav ` /* alt 96 */
         margin-right: 1rem;
         font-size: x-large;
     }
-    .links{
+    .links {
+        position: absolute;
+        top: -700px;
+        left: -2000px;
+        margin-left: auto;
+        margin-right: auto;
+        text-align: center;
+        a{
+            font-size: 2rem;
+            display: block;
+        }
+        @media(min-width: 768px){
+            position: initial;
+            margin: 0;
+            a{
+                font-size: 1rem;
+                display: inline;
+            }
 
+        }    
     }
+    .links.active{
+        width: 100%;
+        display: block;
+        position: absolute;
+        margin-left: auto;
+        margin-right: auto;
+        top: 30%;
+        left: 0;
+        right: 0;
+        text-align: center;
+    }
+    .burger{
+        @media (min-width: 768px){
+            display: none;
+        }
+    }
+
+
 
     
 
